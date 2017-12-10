70ae13451e05556720f72969b7500b0841c633e4
Kevin <kevin.melchert@gmail.com>
2017-12-10 23:42:38 +0100

    age.rb

---
+++ age.rb
@@ -0,0 +1,19 @@
+class Age
+  def initialize(dob)
+    @dob = dob
+  end
+  
+  def now
+    at Time.now
+  end
+  
+  def at(date)
+    (f(date) - f(@dob)) / 10000
+  end
+  
+  private
+  
+  def f(date)
+    date.strftime('%Y%m%d').to_i
+  end
+end
